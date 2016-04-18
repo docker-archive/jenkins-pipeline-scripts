07ad996059b16ef4883de3f22c6c1e370b37beba
Mike Dougherty <mike.dougherty@docker.com>
2016-04-18 11:37:57 -0700

    Add a codecov function which can take a credentials name or infer it from the repository name. It uses the bash script from codecov.io to submit coverage information

---
+++ vars/codecov.groovy
@@ -0,0 +1,14 @@
+def codecov(credsName=null) {
+  if (!credsName) {
+    def path = scm.repositories[0].URIs[0].path
+    def repoOwner = path.split('/')[0]
+    def repoName = path.split('/')[-1].replaceAll(/\.git$/, '')
+    credsName = repoName
+    if (repoOwner != 'docker') {
+      credsName = "${repoOwner}-${credsName}"
+    }
+  }
+  withCredentials([[$class: 'StringBinding', credentialsId: "${credsName}.codecov-token", variable: 'CODECOV_TOKEN']]) {
+    sh "bash <(curl -s https://codecov.io/bash) -t \$CODECOV_TOKEN"
+  }
+}
