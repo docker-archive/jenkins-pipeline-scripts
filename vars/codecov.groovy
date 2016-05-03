007c8d9bc94ee221eb5de1ca63ea19699e09b030
Mike Dougherty <mike.dougherty@docker.com>
2016-05-02 21:29:49 -0700

    codecov: remove unnecessary -t and -x

--- vars/codecov.groovy
+++ vars/codecov.groovy
@@ -20,7 +20,7 @@ def call(credsName=null) {
   // Set some env variables so codecov detection script works correctly
   withEnv(["GIT_BRANCH=${branchName}", "GIT_COMMIT=${gitRevision}", "ghprbPullId=${ghprbPullId}"]) {
     withCredentials([[$class: 'StringBinding', credentialsId: "${credsName}.codecov-token", variable: 'CODECOV_TOKEN']]) {
-      sh "bash -x <(curl -s https://codecov.io/bash) -t \$CODECOV_TOKEN"
+      sh "bash <(curl -s https://codecov.io/bash)"
     }
   }
 }
