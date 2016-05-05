71215124ae1a1391828c7c8bac4f0b56896050f4
Mike Dougherty <mike.dougherty@docker.com>
2016-05-05 11:32:46 -0700

    codecov: Use github API to get branch name for a PR

--- vars/codecov.groovy
+++ vars/codecov.groovy
@@ -1,3 +1,5 @@
+import org.kohsuke.github.GitHub
+
 def call(credsName=null) {
   if (!credsName) {
     def path = scm.repositories[0].URIs[0].path
@@ -9,8 +11,34 @@ def call(credsName=null) {
     }
   }
 
+  def branchName = env.BRANCH_NAME
+  if (env.CHANGE_ID) {
+    def repoUrl = getOutput("git config --get remote.origin.url")
+    // Need to get name from url, supports these variants:
+    //  git@github.com:docker/docker.git -> docker/docker
+    //  git://github.com/docker/docker.git -> docker/docker
+    //  https://github.com/docker/docker.git -> docker/docker
+    //  ssh://git@github.com/docker/docker.git -> docker/docker
+    // 1. split on colon, take the last part.
+    // 2. split that on slash, take the last 2 parts and rejoin them with /.
+    // 3. remove .git at the end
+    // 4. ta-da
+    def repoName = repoUrl.split(":")[-1].split("/")[-2..-1].join("/").replaceAll(/\.git$/, '')
+    def githubToken
+    withCredentials([[
+      variable: "GITHUB_TOKEN",
+      credentialsId: "docker-jenkins.token.github.com",
+      $class: "StringBinding",
+    ]]) {
+      githubToken = getOutput("echo -n \$GITHUB_TOKEN")
+    }
+    def gh = GitHub.connectUsingOAuth(githubToken)
+    def pr = gh.getRepository(repoName).getPullRequest(env.CHANGE_ID.toInteger())
+    branchName = "${pr.head.user.login}/${pr.head.ref}"
+  }
+
   // Set some env variables so codecov detection script works correctly
-  withEnv(["ghprbPullId=${env.CHANGE_ID}"]) {
+  withEnv(["ghprbPullId=${env.CHANGE_ID}", "GIT_BRANCH=${branchName}"]) {
     withCredentials([[$class: 'StringBinding', credentialsId: "${credsName}.codecov-token", variable: 'CODECOV_TOKEN']]) {
       sh 'bash <(curl -s https://codecov.io/bash) -v || echo "codecov exited with \$?"'
     }
