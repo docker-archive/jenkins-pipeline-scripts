af98edff754d95204c9368c02a7541cf7ef2308c
Michael Dougherty <mikedougherty@users.noreply.github.com>
2016-09-12 13:56:11 -0700

    Update wrappedNode.groovy
    somehow, `withDockerRegistry` and `docker.withRegistry` are not the same, even though they're supposed to be.

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -1,7 +1,7 @@
 def call(Map vars, Closure body=null) {
   vars = vars ?: [:] 
   node(vars.get("label", null)) {
-    docker.withRegistry(vars.get("registryUrl", "https://index.docker.io/v1/"), vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
+    withDockerRegistry(vars.get("registryUrl", "https://index.docker.io/v1/"), vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
       wrap([$class: 'TimestamperBuildWrapper']) {
         wrap([$class: 'AnsiColorBuildWrapper']) {
           if (body) { body() }
