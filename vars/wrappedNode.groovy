48d6a2fb9dfaa6f3397171a03b15b08e63879a43
Mike Dougherty <mike.dougherty@docker.com>
2016-09-22 13:41:23 -0700

    Trim whitespace off the end of pwd

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -1,7 +1,7 @@
 def call(Map vars, Closure body=null) {
   vars = vars ?: [:]
   node(vars.get("label", null)) {
-    withEnv(["WORKSPACE=${getOutput('pwd')}"]) {
+    withEnv(["WORKSPACE=${getOutput('pwd').trim()}"]) {
       withDockerRegistry(url: vars.get("registryUrl", "https://index.docker.io/v1/"), credentialsId: vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
         wrap([$class: 'TimestamperBuildWrapper']) {
           wrap([$class: 'AnsiColorBuildWrapper']) {
