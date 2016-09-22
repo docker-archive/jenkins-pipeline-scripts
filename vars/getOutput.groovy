98ef065796cdc8ae5b74f1855041fc187c8b1ff8
Mike Dougherty <mike.dougherty@docker.com>
2016-09-22 12:25:52 -0700

    'sh' has an option for getting the stdout. Replace 'getOutput' implementation with that

--- vars/getOutput.groovy
+++ vars/getOutput.groovy
@@ -1,10 +1,3 @@
 String call(cmd) {
-  def filename = "${env.BUILD_TAG}-getOutput.txt"
-  try {
-    sh "${cmd} > '${filename}'"
-    result = readFile(filename)
-  } finally {
-    sh "rm -f '${filename}'"
-  }
-  return result
+  sh(script: cmd, returnStdout: true)
 }
