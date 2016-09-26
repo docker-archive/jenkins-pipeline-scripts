631eb1a4e56304d1af3e31794efd3c2c43ca4aa9
Mike Dougherty <mike.dougherty@docker.com>
2016-09-26 10:47:56 -0700

    getOutput is deprecated; use 'returnStdout: true'

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -1,7 +1,7 @@
 def call(Map vars, Closure body=null) {
   vars = vars ?: [:]
   node(vars.get("label", null)) {
-    withEnv(["WORKSPACE=${getOutput('pwd').trim()}"]) {
+    withEnv(["WORKSPACE=${sh(script: 'pwd', returnStdout: true).trim()}"]) {
       withDockerRegistry(url: vars.get("registryUrl", "https://index.docker.io/v1/"), credentialsId: vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
         wrap([$class: 'TimestamperBuildWrapper']) {
           wrap([$class: 'AnsiColorBuildWrapper']) {
