289c0b0165b63e0e92e729e01004679a9033babb
Mike Dougherty <mike.dougherty@docker.com>
2016-11-02 12:33:57 -0700

    Implement a 'cleanWorkspace' option for wrappedNode

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -5,6 +5,15 @@ def call(Map vars, Closure body=null) {
       withDockerRegistry(url: vars.get("registryUrl", "https://index.docker.io/v1/"), credentialsId: vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
         wrap([$class: 'TimestamperBuildWrapper']) {
           wrap([$class: 'AnsiColorBuildWrapper']) {
+            if (vars.get('cleanWorkspace', false)) {
+              // NOTE: `withChownWorkspace` uses docker. if our `label` doesn't have docker
+              // or is misconfigured, these operations will fail and the exception will be
+              // propogated.
+              withChownWorkspace { echo "cleanWorkspace: Ensuring workspace is owned by ${env.USER}" }
+              echo "cleanWorkspace: Removing existing workspace"
+              deleteDir()
+              echo "cleanWorkspace: Workspace is clean."
+            }
             if (body) { body() }
           }
         }
