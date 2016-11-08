35dcfdfcdc230a2ba9672f9ce6949aaf863d2e3e
Mike Dougherty <mike.dougherty@docker.com>
2016-11-08 10:50:26 -0800

    WORKSPACE was restored as a default variable - no longer necessary to have this block

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -1,21 +1,19 @@
 def call(Map vars, Closure body=null) {
   vars = vars ?: [:]
   node(vars.get("label", null)) {
-    withEnv(["WORKSPACE=${sh(script: 'pwd', returnStdout: true).trim()}"]) {
-      withDockerRegistry(url: vars.get("registryUrl", "https://index.docker.io/v1/"), credentialsId: vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
-        wrap([$class: 'TimestamperBuildWrapper']) {
-          wrap([$class: 'AnsiColorBuildWrapper']) {
-            if (vars.get('cleanWorkspace', false)) {
-              // NOTE: `withChownWorkspace` uses docker. if our `label` doesn't have docker
-              // or is misconfigured, these operations will fail and the exception will be
-              // propogated.
-              withChownWorkspace { echo "cleanWorkspace: Ensuring workspace is owned by ${env.USER}" }
-              echo "cleanWorkspace: Removing existing workspace"
-              deleteDir()
-              echo "cleanWorkspace: Workspace is clean."
-            }
-            if (body) { body() }
+    withDockerRegistry(url: vars.get("registryUrl", "https://index.docker.io/v1/"), credentialsId: vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
+      wrap([$class: 'TimestamperBuildWrapper']) {
+        wrap([$class: 'AnsiColorBuildWrapper']) {
+          if (vars.get('cleanWorkspace', false)) {
+            // NOTE: `withChownWorkspace` uses docker. if our `label` doesn't have docker
+            // or is misconfigured, these operations will fail and the exception will be
+            // propogated.
+            withChownWorkspace { echo "cleanWorkspace: Ensuring workspace is owned by ${env.USER}" }
+            echo "cleanWorkspace: Removing existing workspace"
+            deleteDir()
+            echo "cleanWorkspace: Workspace is clean."
           }
+          if (body) { body() }
         }
       }
     }
