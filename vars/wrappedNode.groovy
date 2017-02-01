408e50402ce068f7928932798ad7e69ccdebe837
Dominik Hahn <dominikhahn@users.noreply.github.com>
2017-02-01 09:14:16 +0100

    Update wrappedNode.groovy
    Update docker.withRegistry

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -1,7 +1,7 @@
 def call(Map vars, Closure body=null) {
   vars = vars ?: [:]
   node(vars.get("label", null)) {
-    withDockerRegistry(url: vars.get("registryUrl", "https://index.docker.io/v1/"), credentialsId: vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
+    docker.withRegistry(url: vars.get("registryUrl", "https://index.docker.io/v1/"), credentialsId: vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
       wrap([$class: 'TimestamperBuildWrapper']) {
         wrap([$class: 'AnsiColorBuildWrapper']) {
           if (vars.get('cleanWorkspace', false)) {
